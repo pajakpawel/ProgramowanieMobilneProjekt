ccbcca26cb9f24de8ce61ae0dab12bc1a02dd929
Pawel Pajak <pajakpawel@vp.pl>
2018-12-05 20:28:05 +0100

    Updated MainActivity loayout, added RulesActivity and RulesActivity layout

--- app/src/main/java/com/example/pawe/milionerzy/MainActivity.java
+++ app/src/main/java/com/example/pawe/milionerzy/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pawe.milionerzy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,16 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void newGameButtonOnClick(View view)
+    {
+
+    }
+
+    public void rulesButtonOnClick(View view)
+    {
+        startActivity(new Intent(MainActivity.this, RulesActivity.class));
     }
 }
